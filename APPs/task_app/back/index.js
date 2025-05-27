d3e24902bc90368a2f8eefabaff897e7365684ba
altoel <alto.ellya@gmail.com>
2025-05-27 14:33:03 +0900

    crud

--- APPs/task_app/back/index.js
+++ APPs/task_app/back/index.js
@@ -1,4 +1,4 @@
-const PORT = 8000;
+PORT = 8000;
 
 const express = require("express");
 const cors = require("cors");
@@ -8,8 +8,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
-app.get("/", (request, response) => {
-   response.send("Hello Node, This is Root Page!!");
+app.get("/", (req, res) => {
+   res.send("This is the Tasks App Backend");
 });
+
+app.use(require("./routers/getRoutes"));
+app.use(require("./routers/postRoutes"));
+app.use(require("./routers/updateRoutes"));
+app.use(require("./routers/deleteRoutes"));
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
